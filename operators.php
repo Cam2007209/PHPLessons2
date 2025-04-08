5c732e262181a8bbf1e6eb784ada4c6cbb02b7b3
Cam2007209 <142239366+Cam2007209@users.noreply.github.com>
2025-04-08 09:36:57 -0500

    Create operators.php

---
+++ operators.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //Arithmetic Operators
+        echo 8**2;        
+    ?>
+    <?php
+        //Assignment Operators
+        $x= 50;
+        $x/=5;
+        echo $x;
+    ?>
+    <?php
+        //Comparison Operators
+        $x=2;
+        $y=4;
+
+        if($x==$y){
+            echo "True!";
+        }
+        else{
+            echo"False!";
+        }
+    ?>
+    <?php
+        //Increment/Decrement Operators
+        $x=20;
+        echo --$x;
+        echo $x;
+    ?>
+    <?php
+        //Logical Operators
+        $x=30;
+        $y=50;
+
+        if($x==$y xor 17==17 ){
+            echo"True";
+        }
+    ?>
+</body>
+</html>
