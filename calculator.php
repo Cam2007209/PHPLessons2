35ec43cdbf45c3554b898a7c6baa1154140dc878
Cam2007209 <142239366+Cam2007209@users.noreply.github.com>
2025-04-09 10:07:46 -0500

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -4,8 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+body{
+    background-color:black;
+    color:white;
+}
+
+    </style>
 </head>
 <body>
+<nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="Calendar.php">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="Calendar.php">Calendar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Calculator.php">Calculator</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            More Pages
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="datatypes.php">Data Types</a></li>
+            <li><a class="dropdown-item" href="operators.php">Operators</a></li>
+            <li><a class="dropdown-item" href="index.php">Index</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     <form>
         <input type="text" name="num1" placeholder="Number 1">
         <input type="text" name="num2" placeholder="Number 2">
@@ -17,7 +53,7 @@
             <option>Divide</option>
         </select>
         <br>
-        <button type="submit" name="submit" value="submit">Calculate</button>
+        <button type="submit"  name="submit" value="submit" class="btn btn-outline-light">Calculate</button>
     </form>    
     <p>The answer is:</p>
     <?php
@@ -43,5 +79,6 @@
             }
         }
     ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
