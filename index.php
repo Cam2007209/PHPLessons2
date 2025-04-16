cb28eab8eaf530447dbfcb8b87a9a7c6c70aaaf7
Cam2007209 <142239366+Cam2007209@users.noreply.github.com>
2025-04-16 12:37:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,47 +14,16 @@ body{
     </style>
   </head>
   <body>
-  <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="header.php">Header</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="Calendar.php">Calendar</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="Calculator.php">Calculator</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            More Pages
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="datatypes.php">Data Types</a></li>
-            <li><a class="dropdown-item" href="operators.php">Operators</a></li>
-            <li><a class="dropdown-item" href="index.php">Index</a></li>
-            <li><a class="dropdown-item" href="arrays.php">Arrays</a></li>
-            <li><a class="dropdown-item" href="loops.php">Loops</a></li>
-            <li><a class="dropdown-item" href="functions.php">Functions</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
     <form method="GET">
       <input type="text"name="person">
       <button type="submit" name="submit" class="btn btn-light">Submit</button>
     </form>
     <?php
+    include 'Includes/header.php';
     //This gets something from the database
     $name=$_GET['person'];
     //This is the printed text
     echo $name. " is very smart.";
     ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
